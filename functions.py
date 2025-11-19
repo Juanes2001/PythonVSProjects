8184d958e8fb24bb399e394c62728d22f61ac19f
juanes <jrodriguezoc@unal.edu.co>
2025-11-18 21:54:21 -0500

    c

--- functions.py
+++ functions.py
@@ -6,6 +6,10 @@ Se definiran aqui las funciones que se necesiten para el trabajo para ser luego 
 # Librerias a usar
 import numpy as np
 import sympy as sp
+
+import sympy.vector as vec
+from sympy.vector import CoordSys3D
+
 from IPython.display import display, Math
 import scipy.integrate as integ
 import matplotlib.pyplot as plt
@@ -15,7 +19,23 @@ import math
 # Se definiran los parametros iniciales
 
 u0r = 1
-u0 = 4
+u0 = 4*math.pi* 10e-7
 e0r = 1
 e0 = 8.854e-12 
+
+
+r = CoordSys3D('r')
+E = CoordSys3D('E')
+Ax,Ay,phix,phiy,t = sp.symbols('Ax Ay phix phiy t')
+
+# Definición en forma simbólica de la forma incial de la onda electromagnética la cual tendremos que solucionar en sus variables.
+
+def Evec(t,omega,k):
+
+    phase = sp.exp(sp.I*(r.dot(k)-omega*t))
+    E0_vec = Ax*sp.exp(sp.I*(phix))*E.i + Ay*sp.exp(sp.I*(phiy))*E.j 
+
+    return E0_vec * phase
+
+
 
