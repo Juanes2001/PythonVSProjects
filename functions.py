806f795204ffe6da2aaabb89a07a66b20acc8c45
juanes <jrodriguezoc@unal.edu.co>
2025-11-18 20:30:34 -0500

    c

---
+++ functions.py
@@ -0,0 +1,21 @@
+"""
+Se definiran aqui las funciones que se necesiten para el trabajo para ser luego usadas en en la terminal principal
+"""
+
+
+# Librerias a usar
+import numpy as np
+import sympy as sp
+from IPython.display import display, Math
+import scipy.integrate as integ
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+
+# Se definiran los parametros iniciales
+
+u0r = 1
+u0 = 4
+e0r = 1
+e0 = 8.854e-12 
+
